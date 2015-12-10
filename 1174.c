dcc4d1b16131e04dcc56a93a6007306289e19dd3
Vitor Amaral <vitor.gop@hotmail.com>
2015-12-09 23:47:30 -0200

    Linguagem C

---
+++ 1174.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  
+int main() {
+
+ float a[100];
+ int i;
+
+  for(i = 0; i < 100; i++){
+    scanf("%f", &a[i]);
+  }
+ 
+  for(i = 0; i < 100; i++){
+   if(a[i] <= 10){
+     printf("A[%d] = %.1lf\n", i, a[i]);
+   }
+  }
+    return 0;
+}
