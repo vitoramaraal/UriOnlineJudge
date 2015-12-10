0373d2bd9884fb7ad53fed1078a4a10d6de8d168
Vitor Amaral <vitor.gop@hotmail.com>
2015-12-09 23:27:48 -0200

    Linguagem C

--- 1001.c
+++ 1001.c
@@ -4,10 +4,12 @@ int main() {
 
  int A, B, X;
  
-scanf("%d", &A);
-scanf("%d", &B);
-X = A+B;
-printf("X = %d\n", X);
+  scanf("%d", &A);
+  scanf("%d", &B);
+  
+   X = A+B;
+
+   printf("X = %d\n", X);
  
     return 0;
 }
